9907b5821c55e1ab6fbf22b54a7349c700aab65a
PotoZeko <142264692+PotoZeko@users.noreply.github.com>
2023-08-14 16:05:06 -0400

    Archivos iniciales del proyecto de MakeCode

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "song":return hex`0078000408030102001c000c960064006d019001000478002c010000640032000078000a0100052a000000040001201000140001201400180001221c002000012424002800011b38003c00011d4c005000011e`;
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
